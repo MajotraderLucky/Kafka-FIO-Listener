46d54044ddeff8fe4bd91ab7eeb619fe6adfdc36
MajotraderLucky <majohead@gmail.com>
2024-01-22 18:22:39 +0300

    feat(main): Add loogger in to the main.go file

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/MajotraderLucky/Utils/logger"
 )
 
 func main() {
@@ -10,4 +13,18 @@ func main() {
 	time.Sleep(time.Second * 10)
 
 	fmt.Println("Hello, world!")
+
+	logger := logger.Logger{}
+	err := logger.CreateLogsDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = logger.OpenLogFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger.SetLogger()
+	logger.LogLine()
+
+	log.Println("Hello, logger!")
 }
