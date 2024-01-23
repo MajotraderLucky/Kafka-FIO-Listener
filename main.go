6dfdb7534cb815e5d7ee075d05b604bba5b3175a
MajotraderLucky <majohead@gmail.com>
2024-01-23 19:46:42 +0300

    feat(main): Add connectAndCheckDB function

--- main.go
+++ main.go
@@ -12,11 +12,38 @@ import (
 	"github.com/MajotraderLucky/Utils/logger"
 )
 
+func connectAndCheckDB() (*sql.DB, error) {
+	// Подключение к базе данных
+	db, err := sql.Open("postgres", "host=db port=5432 user=postgres password=mysecretpassword dbname=mydatabase sslmode=disable")
+	if err != nil {
+		log.Println("ошибка при подключении к базе данных: ", err)
+		return nil, fmt.Errorf("ошибка при подключении к базе данных: %w", err)
+	}
+
+	// Проверка наличия базы данных
+	_, err = db.Exec("SELECT 1")
+	if err != nil {
+		log.Println("база данных не существует: ", err)
+		return nil, fmt.Errorf("база данных не существует: %w", err)
+	} else {
+		log.Println("база данных существует")
+	}
+
+	// Проверка наличия таблицы
+	_, err = db.Exec("SELECT 1 FROM fio_data LIMIT 1")
+	if err != nil {
+		log.Println("таблица не существует: ", err)
+		return nil, fmt.Errorf("таблица не существует: %w", err)
+	} else {
+		log.Println("таблица существует")
+	}
+
+	return db, nil
+}
+
 func main() {
 	// Wait for other services to be ready
 	time.Sleep(time.Second * 10)
-
-	fmt.Println("Hello, world!")
 
 	logger := logger.Logger{}
 	err := logger.CreateLogsDir()
@@ -32,28 +59,14 @@ func main() {
 
 	log.Println("Hello, logger!")
 
-	// Connect to the database
-	db, err := sql.Open("postgres", "host=db port=5432 user=postgres password=mysecretpassword dbname=mydatabase sslmode=disable")
+	// Connect to the database and check it
+	db, err := connectAndCheckDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Check if the database exists
-	_, err = db.Exec("SELECT 1")
-	if err != nil {
-		log.Fatal("Database does not exist: ", err)
-	} else {
-		log.Println("Database exists")
-	}
-
-	// Check if the table exists
-	_, err = db.Exec("SELECT 1 FROM fio_data LIMIT 1")
-	if err != nil {
-		log.Fatal("Table does not exist: ", err)
-	} else {
-		log.Println("Table exists")
-	}
-
 	// Close the database connection
 	defer db.Close()
+
+	logger.CleanLogCountLines(50)
 }
