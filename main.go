8c0f0c482704aa5b90be6c628302dc601c07c9cb
MajotraderLucky <majohead@gmail.com>
2024-02-03 05:00:26 +0300

    refactor(main): Move the CheckZookeeper function to the checker package

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
@@ -9,62 +8,7 @@ import (
 
 	"github.com/MajotraderLucky/Kafka-FIO-Listener/checker"
 	"github.com/MajotraderLucky/Utils/logger"
-	zk "github.com/go-zookeeper/zk"
 )
-
-// CheckZookeeper checks the connection and operations with Zookeeper
-func CheckZookeeper() error {
-	conn, _, err := zk.Connect([]string{"zookeeper:2181"}, 10*time.Second)
-	if err != nil {
-		log.Println("Failed to connect to Zookeeper:", err)
-		return fmt.Errorf("failed to connect to Zookeeper: %s", err)
-	}
-	defer conn.Close()
-
-	_, err = conn.Create("/test", []byte("test"), 0, zk.WorldACL(zk.PermAll))
-	if err != nil {
-		log.Println("Failed to create test node:", err)
-		return fmt.Errorf("failed to create test node: %s", err)
-	}
-
-	defer func() {
-		conn.Delete("/test", -1)
-	}()
-
-	data, stat, err := conn.Get("/test")
-	if err != nil {
-		log.Println("Failed to get data from test node:", err)
-		return fmt.Errorf("failed to get data from test node: %s", err)
-	}
-
-	if string(data) != "test" {
-		log.Println("znode content does not match expected:", data)
-		return fmt.Errorf("znode content does not match expected: %s", data)
-	}
-
-	if stat.Cversion != 0 {
-		log.Println("node version is not zero:", stat.Cversion)
-		return fmt.Errorf("node version is not zero: %d", stat.Cversion)
-	}
-
-	eventChan := make(chan zk.Event)
-	go func() {
-		eventChan <- zk.Event{Type: zk.EventSession}
-	}()
-
-	select {
-	case event := <-eventChan:
-		if event.Type != zk.EventSession {
-			log.Println("expected EventSession type event, got:", event.Type)
-			return fmt.Errorf("expected EventSession type event, got: %v", event.Type)
-		}
-	case <-time.After(time.Second * 10):
-		log.Println("timeout while waiting for an event")
-		return fmt.Errorf("timeout while waiting for an event")
-	}
-
-	return nil
-}
 
 func main() {
 	// Wait for other services to be ready
@@ -93,7 +37,7 @@ func main() {
 	// Close the database connection
 	defer db.Close()
 
-	err = CheckZookeeper()
+	err = checker.CheckZookeeper()
 	if err != nil {
 		log.Fatalf("Error checking Zookeeper: %s", err)
 	}
