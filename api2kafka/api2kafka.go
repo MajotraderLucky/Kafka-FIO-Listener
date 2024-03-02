2ff8e0e19fa8f37b68137816f1c18783dc73aa36
MajotraderLucky <majohead@gmail.com>
2024-03-02 14:34:05 +0300

    feat(api2kafka): Wrote the logic of the API in the api2kafka.go file and change the sequence of loading api2kafka service in docker-compose.yml file - all services load without errors

--- api2kafka/api2kafka.go
+++ api2kafka/api2kafka.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
 
 	"github.com/MajotraderLucky/Utils/logger"
+
+	"github.com/gofiber/fiber/v2"
 )
+
+// Structure to store the response from the API
+type ApiResponse struct {
+	Data string `json:"data"`
+}
+
+func fetchDataFromAPI(url string) (string, error) {
+	// Send GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	// Read response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	// Parse JSON
+	var apiResponse ApiResponse
+	err = json.Unmarshal(body, &apiResponse)
+	if err != nil {
+		return "", err
+	}
+	return apiResponse.Data, nil
+}
 
 func main() {
 	logger := logger.Logger{}
@@ -20,4 +51,21 @@ func main() {
 	logger.LogLine()
 	log.Println("API2Kafka is starting...")
 	logger.LogLine()
+
+	app := fiber.New()
+
+	app.Get("data", func(c *fiber.Ctx) error {
+		// URL of the open API
+		url := "http://127.0.0.1:8086/data"
+
+		data, err := fetchDataFromAPI(url)
+		if err != nil {
+			log.Println(err)
+			return c.Status(500).SendString("Internal server error")
+		}
+		// Display the result
+		return c.SendString(data)
+	})
+	// Start server
+	app.Listen(":3000")
 }
